7f26ed0f72bd4956f778c4a52c3753b55d9ca4c2
liwenxiang <liwenx97@163.com>
2020-07-22 17:28:08 +0800

    集成eureka实现高可用集群

--- config_server/src/main/java/com/maowudi/config_server/ConfigTestServerApplication.java
+++ config_server/src/main/java/com/maowudi/config_server/ConfigTestServerApplication.java
@@ -3,9 +3,11 @@ package com.maowudi.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigTestServerApplication {
 
     public static void main(String[] args) {
